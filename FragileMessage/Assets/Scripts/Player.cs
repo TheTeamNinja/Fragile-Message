339b38a344edc179f325fe55f95460ef0cade7d8
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2018-01-26 23:05:02 -0400

    Player tilt and velocity

--- FragileMessage/Assets/Scripts/Player.cs
+++ FragileMessage/Assets/Scripts/Player.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
 public class Player : MonoBehaviour
 {
+    public Boundary boundary;
+    public float speed;
+    public float tilt;
+    public Rigidbody rigidBody;
 
-	public float speed;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		
-		transform.Translate(translation, 0, 0);
-	}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rigidBody.velocity = movement * speed;
+        rigidBody.position = new Vector3(Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax), 0, 0);
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
+    }
 }
