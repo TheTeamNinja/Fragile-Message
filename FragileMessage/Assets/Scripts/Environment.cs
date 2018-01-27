275c1d30f6d577a59e92f46127ff49bafa8d6008
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2018-01-26 21:40:14 -0400

    Basic movement and environment scroll

---
+++ FragileMessage/Assets/Scripts/Environment.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour {
+
+	public float scrollSpeed;
+	public GameObject[] envBlocks;
+	public Transform spawnTransform;
+
+	private float blockSize = 15.0f;
+	private GameObject blockA;
+	private GameObject blockB;
+	private GameObject blockC;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		blockA = CreateBlock(spawnTransform, -2);
+		blockB = CreateBlock(spawnTransform, -1);
+		blockC = CreateBlock(spawnTransform, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		blockA.transform.Translate(0, 0, -1 * scrollSpeed * Time.deltaTime);
+		blockB.transform.Translate(0, 0, -1 * scrollSpeed * Time.deltaTime);
+		blockC.transform.Translate(0, 0, -1 * scrollSpeed * Time.deltaTime);
+
+		if (blockA.transform.position.z <= -blockSize)
+		{
+			SwapBlocks();
+		}
+	}
+
+	private void SwapBlocks()
+	{
+		Debug.Log("Swapping blocks...");
+
+		var deleteMeBlock = blockA;
+		blockA = blockB;
+		blockB = blockC;
+		blockC = CreateBlock(blockB.transform, 1);
+		
+		Destroy(deleteMeBlock);
+	}
+
+	private GameObject CreateBlock(Transform pSpawnTransform, int offset)
+	{
+		GameObject original = envBlocks[Random.Range(0, envBlocks.Length)];
+		
+		Vector3 spawnPosition = pSpawnTransform.position + new Vector3(0, 0, blockSize) * offset;
+		Quaternion spawnRotation = pSpawnTransform.rotation;
+		
+		GameObject clone = Instantiate (original, spawnPosition, spawnRotation);
+
+		return clone;
+	}
+}
---
+++ FragileMessage/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		
+		transform.Translate(translation, 0, 0);
+	}
+}
