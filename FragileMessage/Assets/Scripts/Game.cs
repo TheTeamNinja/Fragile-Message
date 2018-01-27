afd6199263a26ecc345c736df725290d8435bc9e
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2018-01-27 17:07:29 -0400

    High Score

--- FragileMessage/Assets/Scripts/Game.cs
+++ FragileMessage/Assets/Scripts/Game.cs
@@ -4,99 +4,115 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Game : Singleton<Game> {
+public class Game : Singleton<Game>
+{
+    public float planeSpeed;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public GameObject[] cars;
+    public Transform[] spawnCars;
+    public GameObject cameraIntro;
+    public GameObject cameraGameplay;
+    public GameObject startPanel;
+    public GameObject gameoverPanel;
+    public GameObject gamePanel;
+    public Player player;
+    public Environment environment;
+    public Text scoreText;
+    public Text highScoreText;
+    public Text yourScoreText;
+    public bool playing;
 
-	public float planeSpeed;
-	public int hazardCount;
-	public float spawnWait;
-	public float startWait;
-	public float waveWait;
-	public GameObject[] cars;
-	public Transform[] spawnCars;
-	public GameObject cameraIntro;
-	public GameObject cameraGameplay;
-	public GameObject startPanel;
-	public GameObject gameoverPanel;
-	public GameObject gamePanel;
-	public Player player;
-	public Environment environment;
-	public Text scoreText;
-	public bool playing;
-	
-	private int score;
-	
-	void Start ()
-	{
-		InitGame();
-	}
+    private int score;
+    private int highScore;
 
-	public void InitGame()
-	{
-		startPanel.SetActive(true);
-		gameoverPanel.SetActive(false);
-		gamePanel.SetActive(false);
-		playing = false;
-		cameraIntro.SetActive (true);
-		cameraGameplay.SetActive (false);
-	}
-	
-	IEnumerator SpawnWaves() {
-		yield return new WaitForSeconds (startWait);
+    void Start()
+    {
+        InitGame();
+    }
 
-		while (playing) {
-			for (int i = 0; i < hazardCount; i++) {
-				GameObject hazard = cars[Random.Range(0, cars.Length)];
-				Transform spawnCar = spawnCars[Random.Range(0, spawnCars.Length)];
-				
-				Instantiate (hazard, spawnCar.position, spawnCar.rotation);
+    public void InitGame()
+    {
+        startPanel.SetActive(true);
+        gameoverPanel.SetActive(false);
+        gamePanel.SetActive(false);
+        playing = false;
+        cameraIntro.SetActive(true);
+        cameraGameplay.SetActive(false);
+    }
 
-				yield return new WaitForSeconds (spawnWait);
-			}
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
 
-			yield return new WaitForSeconds (waveWait);
-		}
-	}
+        while (playing)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = cars[Random.Range(0, cars.Length)];
+                Transform spawnCar = spawnCars[Random.Range(0, spawnCars.Length)];
 
-	public void GameIntro()
-	{
-		score = 0;
-		scoreText.text = score.ToString();
-		startPanel.SetActive(false);
-		playing = false;
-		player.PlayIntro();
-	}
+                Instantiate(hazard, spawnCar.position, spawnCar.rotation);
 
-	public void StartGame()
-	{
-		gamePanel.SetActive(true);
-		cameraIntro.SetActive (false);
-		cameraGameplay.SetActive (true);
-		playing = true;
-		planeSpeed = 5;
-		StartCoroutine (SpawnWaves ());
-	}
+                yield return new WaitForSeconds(spawnWait);
+            }
 
-	public void GameOver()
-	{
-		gamePanel.SetActive(false);
-		gameoverPanel.SetActive(true);
-		planeSpeed = 0;
-		playing = false;
-	}
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
 
-	public void RestartGame()
-	{
-		environment.InitGame();
-		gameoverPanel.SetActive(false);
-		player.Restart();
-		GameIntro();
-		cameraGameplay.SetActive (false);
-		cameraIntro.SetActive (true);
-	}
+    public void GameIntro()
+    {
+        highScore = PlayerPrefs.GetInt("highScore", highScore);
+        score = 0;
+        scoreText.text = score.ToString();
+        startPanel.SetActive(false);
+        playing = false;
+        player.PlayIntro();
+    }
 
-	public void AddScore(int points)
-	{
-		score += points;
-		scoreText.text = score.ToString();
-	}
+    public void StartGame()
+    {
+        gamePanel.SetActive(true);
+        cameraIntro.SetActive(false);
+        cameraGameplay.SetActive(true);
+        playing = true;
+        planeSpeed = 5;
+        StartCoroutine(SpawnWaves());
+    }
+
+    public void GameOver()
+    {
+        gamePanel.SetActive(false);
+        gameoverPanel.SetActive(true);
+        planeSpeed = 0;
+        playing = false;
+
+        if (score > highScore)
+        {
+            highScore = score;
+
+            PlayerPrefs.SetInt("highScore", highScore);
+        }
+        highScoreText.text = "High Score: " + highScore;
+        yourScoreText.text = "Your Score: " + score;
+    }
+
+    public void RestartGame()
+    {
+        environment.InitGame();
+        gameoverPanel.SetActive(false);
+        player.Restart();
+        GameIntro();
+        cameraGameplay.SetActive(false);
+        cameraIntro.SetActive(true);
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+    }
 }
